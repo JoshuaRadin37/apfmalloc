1a1ccb345ffcf35a148ee7b8044b162cdcf7081b
Joshua Radin <jradin16@gmail.com>
2020-06-05 13:57:38 -0400

    Updated trivial test to be less annoying

--- tests/trivial.rs
+++ tests/trivial.rs
@@ -8,13 +8,13 @@ fn run() {
     unsafe {
         let o = (do_malloc(size_of::<Option<usize>>()) as *mut MaybeUninit<Option<usize>>);
         assert_ne!(o, null_mut());
-        println!("First allocation successful");
+        // println!("First allocation successful");
         *o = MaybeUninit::new(Some(15));
         let o = o as *mut Option<usize>;
 
         do_malloc(size_of::<[usize; 64]>());
         assert_ne!(o, null_mut());
-        println!("First allocation successful");
+        // println!("First allocation successful");
 
         do_free(o as *const Option<usize>);
     }
