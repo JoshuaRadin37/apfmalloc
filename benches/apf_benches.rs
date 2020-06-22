2c6c13420c0ac0939d5487b8e33c8d3bd3cc76db
eliasnd <elias@ndcoding.com>
2020-06-22 16:38:43 -0400

    Benches for all apf functions

--- benches/apf_benches.rs
+++ benches/apf_benches.rs
@@ -1,6 +1,8 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{ criterion_group, criterion_main, BenchmarkId, Criterion, Throughput };
 use lrmalloc_rs::apf::trace::{ Trace, Event };
 use lrmalloc_rs::apf::histogram::Histogram;
+use lrmalloc_rs::apf::timescale_functions::{ LivenessCounter, ReuseCounter };
+use lrmalloc_rs::apf::ApfTuner;
 
 fn trace_add(c: &mut Criterion) {
     let mut t = Trace::new();
@@ -29,10 +31,87 @@ fn histogram_add(c: &mut Criterion) {
     });
 }
 
+fn liveness_counter_alloc(c: &mut Criterion) {
+    let mut lc = LivenessCounter::new();
+    c.bench_function("lc alloc", |b| {
+        b.iter(|| {
+            lc.inc_timer();
+            lc.alloc();
+        });
+    });
+}
+
+fn liveness_counter_free(c: &mut Criterion) {
+    let mut lc = LivenessCounter::new();
+    c.bench_function("lc free", |b| {
+        b.iter(|| {
+            lc.free();
+        });
+    });
+}
+
+fn reuse_counter_alloc(c: &mut Criterion) {
+    let mut rc = ReuseCounter::new(20000, 40000);
+    c.bench_function("rc alloc", |b| {
+        b.iter(|| {
+            rc.alloc(0);
+            rc.inc_timer();
+        });
+    });
+}
+
+fn reuse_counter_free(c: &mut Criterion) {
+    let mut rc = ReuseCounter::new(20000, 40000);
+    c.bench_function("rc free", |b| {
+        b.iter(|| {
+            rc.free(0);
+        });
+    });
+}
+
+fn check(id: usize) -> u32 {
+    0
+}
+
+fn get(id: usize, val: usize) -> bool {
+    true
+}
+
+fn ret(id: usize, val: u32) -> bool {
+    true
+}
+
+fn apf_tuner_alloc(c: &mut Criterion) {
+    let mut apf = ApfTuner::new(0, check, get, ret);
+
+    c.bench_function("apf alloc", |b| {
+        b.iter(|| {
+            apf.malloc(0 as *mut u8);
+        });
+    });
+}
+
+fn apf_tuner_free(c: &mut Criterion) {
+    let mut apf = ApfTuner::new(0, check, get, ret);
+    apf.malloc(0 as *mut u8);
+
+    c.bench_function("apf free", |b| {
+        b.iter(|| {
+            apf.free(0 as *mut u8);
+        });
+    });
+}
+
 criterion_group!(
     apf_functions,
     trace_add,
     histogram_inc,
-    histogram_add
+    histogram_add,
+    liveness_counter_alloc,
+    liveness_counter_free,
+    reuse_counter_alloc,
+    reuse_counter_free,
+    apf_tuner_alloc,
+    apf_tuner_free
 );
 criterion_main!(apf_functions);
