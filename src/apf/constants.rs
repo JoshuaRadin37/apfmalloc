36958e1016fbcd05104536d8898eb2b1e45a75fb
Joshua Radin <jradin16@gmail.com>
2020-08-13 12:51:25 -0400

    Converted all environmental variable checks to non-heap-allocating checks

--- src/apf/constants.rs
+++ src/apf/constants.rs
@@ -7,9 +7,7 @@ pub const USE_ALLOCATION_CLOCK: bool = true;
 use crate::thread_cache::no_tuning;
 use crate::env::{get_env_as_usize};
 lazy_static::lazy_static! {
-pub static ref TARGET_APF: usize = no_tuning(
-        || get_env_as_usize("TARGET_APF").unwrap_or(2500)
-    );
-pub static ref REUSE_BURST_LENGTH: usize = no_tuning(|| std::env::var("BURST_LENGTH").map(|apf| apf.parse::<usize>().unwrap_or(300)).unwrap_or(300));
-pub static ref REUSE_HIBERNATION_PERIOD: usize = no_tuning(|| std::env::var("HIBERNATION_PERIOD").map(|apf| apf.parse::<usize>().unwrap_or(*REUSE_BURST_LENGTH*2)).unwrap_or(*REUSE_BURST_LENGTH*2));
+pub static ref TARGET_APF: usize = no_tuning(|| get_env_as_usize("TARGET_APF").unwrap_or(2500));
+pub static ref REUSE_BURST_LENGTH: usize = no_tuning(|| get_env_as_usize("BURST_LENGTH").unwrap_or(300));
+pub static ref REUSE_HIBERNATION_PERIOD: usize = no_tuning(|| get_env_as_usize("HIBERNATION_PERIOD").unwrap_or(*REUSE_BURST_LENGTH*2));
 }
